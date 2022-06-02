b84ced259ae59a674898ff0ea4439c421a67a344
Hùng Nguyễn <100525071+n-hungdev@users.noreply.github.com>
2022-06-02 18:22:14 +0700

    first commit

---
+++ src/components/Hello.tsx
@@ -0,0 +1,10 @@
+const Hello = () => {
+  return (
+    <>
+      <h1 className='text-center'>Hello Word</h1>
+      <p className='text-center'>Vite JS + Typescript + Eslint + Prettier</p>
+    </>
+  );
+};
+
+export default Hello;
